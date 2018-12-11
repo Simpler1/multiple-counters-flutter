75ed814b0526da072f2ae7261858b181fdd39cde
Simpler1 <kevin.bezold@gmail.com>
2018-12-11 13:51:42 -0500

    Comment for ListItemsBuilder

--- lib/common_widgets/list_items_builder.dart
+++ lib/common_widgets/list_items_builder.dart
@@ -3,6 +3,10 @@ import 'package:multiple_counters_flutter/common_widgets/placeholder_content.dar
 
 typedef Widget ItemWidgetBuilder<T>(BuildContext context, T item);
 
+/// A generic list builder which returns either:
+///   - A circular progress indicator when items is null
+///   - A PlaceholderContent when items is zero
+///   - A list of items otherwise
 class ListItemsBuilder<T> extends StatelessWidget {
   ListItemsBuilder({this.items, this.itemBuilder});
   final List<T> items;
